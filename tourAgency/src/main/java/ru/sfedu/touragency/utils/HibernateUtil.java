3225718fc6c5119ea2642467383a4b71b9993b5c
artem1y3 <artem1y@outlook.com>
2019-03-03 15:16:53 +0300

    Next commit

---
+++ tourAgency/src/main/java/ru/sfedu/touragency/utils/HibernateUtil.java
@@ -0,0 +1,32 @@
+package ru.sfedu.touragency.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+    /**
+     * Создание фабрики
+     *
+     */
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            // loads configuration and mappings
+            Configuration configuration = new Configuration().configure();
+            ServiceRegistry serviceRegistry
+                    = new StandardServiceRegistryBuilder()
+                    .applySettings(configuration.getProperties()).build();
+
+
+            MetadataSources metadataSources =
+                    new MetadataSources(serviceRegistry);
+            metadataSources.addAnnotatedClass(TestEntity.class);// Аннотированная сущность
+            metadataSources.addResource("named-queries.hbm.xml");// Именованные запросы
+            sessionFactory = metadataSources.buildMetadata().buildSessionFactory();
+        }
+
+        return sessionFactory;
+    }
+}
